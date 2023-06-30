83370ad519247d477f98f44495fd9a76d7ad3d5d
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-06-30 10:32:48 +0530

    documented free groups

--- GroupComp/WordProblem/FreeGroup.lean
+++ GroupComp/WordProblem/FreeGroup.lean
@@ -4,7 +4,16 @@ import Mathlib
 
 We translate into Lean the solution to the word problem in free groups based on covering spaces. We use the same definitions and some of the lemmas from the solution in Mathlib4.
 
-The translation is not faithful - we skip definitions of graphs and paths and instead abstract the sequence of vertices along the lift of a path.
+The translation is not faithful - we skip definitions of graphs and paths and instead identify lifts of paths with sequences of vertices. 
+
+The vertices in the universal cover are reduced words in the generators of the free group. A path in wedge of circles is a word in the generators and their inverses. 
+
+* We define the lift of a path as a sequence of vertices.
+* We show that the vertices along the path are reduced.
+* We show that a reduced word is lifted to itself.
+* We show that the lift of a word is equal in the free group to the terminal vertex of its lift.
+
+All this is done by defining and proving properties of the lift on an edge starting at a (reduced) word. The lift of a path is then defined as the concatenation of the lifts of its edges.
 -/
 
 
@@ -22,15 +31,33 @@ open Red
 
 #check not_step_nil -- FreeGroup.Red.not_step_nil.{u} {α : Type u} {L : List (α × Bool)} : ¬Step [] L
 
+/--
+Proposition that a word is reduced.
+-/
 abbrev Reduced (L: List (α × Bool)) : Prop := ∀ L' : List (α × Bool), ¬ Step L L' 
 
+/--
+The empty word is reduced.
+-/
 theorem reduced_nil : @Reduced α ([]) := by apply not_step_nil
 
+/--
+A word with a single letter is reduced.
+-/
 theorem reduced_singleton (p : α × Bool) : @Reduced α ([p]) := by 
   intro L' step
   apply not_step_singleton step  
  
+/--
+The inverse of a letter.
+-/
 abbrev Letter.inv (p : α × Bool) : α × Bool := (p.1, !p.2)
+
+instance : Inv (α × Bool) := ⟨Letter.inv⟩
+
+theorem inv_def (p : α × Bool) : p⁻¹ = (p.1, !p.2) := rfl
+
+theorem inv_def' (l : α)(b : Bool) : (l, b)⁻¹ = (l, !b) := rfl
 
 end FreeGroup
 
@@ -38,7 +65,9 @@ end FreeGroup
 
 #check List.of_concat_eq_concat -- List.of_concat_eq_concat.{u} {α : Type u} {as bs : List α} {a b : α} (cancP : List.concat as a = List.concat bs b) : as = bs ∧ a = b
 
-
+/--
+For a non-empty list, appending the last element to the list obtained by `dropLast` is the same as the original list.
+-/
 theorem split_drop (l: List β)(p : l ≠ []) :
     l.dropLast ++ [l.getLast p] = l := by
     match l with
@@ -51,6 +80,9 @@ theorem split_drop (l: List β)(p : l ≠ []) :
 namespace FreeGroup
 open Red
 
+/--
+The next vertex in the lift of a path with given initial word and edge to lift. The definition does not require the initial word to be reduced, but the properties do.
+-/
 def nextVertex (start: List (α × Bool))(letter: (α × Bool)) : List (α × Bool) := 
   if nilP:start = [] then [letter]
   else
@@ -62,21 +94,33 @@ def nextVertex (start: List (α × Bool))(letter: (α × Bool)) : List (α × Bo
 
 infixl:65 " :+ " => nextVertex
 
+/--
+Lift of an edge at the empty word.
+-/
 theorem nil_nextVertex (letter : (α × Bool)) : 
     nextVertex [] letter = [letter] := by rfl
 
+/--
+Lift of an edge where we do not cancel the last letter.
+-/
 theorem nonnil_nextVertex_extend 
     (start : List (α × Bool)) (nonnil: start ≠ []) (letter : (α × Bool))
     (neq : start.getLast nonnil ≠ Letter.inv letter) : 
     start :+ letter = start.concat letter := by 
   simp [nextVertex,  neq, if_neg, nonnil]
 
+/--
+Lift of an edge where we cancel the last letter.
+-/
 theorem nonnil_nextVertex_cancel (start : List (α × Bool)) 
     (nonnil: start ≠ []) (letter : (α × Bool)) 
     (eql : start.getLast nonnil = Letter.inv letter) : 
     start :+ letter = start.dropLast  := by 
   simp [nextVertex,  eql, if_pos, nonnil]
 
+/--
+To avoid subtleties with indices, we extract from a step the letter and the two parts of the list around it as well as the relation.
+-/
 theorem exists_of_step {L L': List (α × Bool)} :
     Step L L' → ∃ x : α , ∃  b : Bool, ∃ L₁ L₂ : List (α × Bool ),
     L = (L₁ ++ (x, b) :: (x, !b) :: L₂) ∧ L' = L₁ ++ L₂  := by
@@ -84,6 +128,9 @@ theorem exists_of_step {L L': List (α × Bool)} :
   induction step with
   | @not L₁ L₂ x b =>  use x, b, L₁, L₂; simp
 
+/--
+If we lift an edge starting at a reduced word, the result is reduced.
+-/
 theorem nextVertex_reduced (start: List (α × Bool))(letter: (α × Bool)):
     Reduced start → Reduced (start :+ letter) := by
   intro reduced_start
@@ -134,10 +181,16 @@ theorem nextVertex_reduced (start: List (α × Bool))(letter: (α × Bool)):
         rw [eq1'']
         apply Step.not
 
+/--
+Words are equivalent if they represent the same element of the free group.
+-/
 abbrev equiv (a b : List (α × Bool)) := mk a = mk b
 
 infix:50 " ∼ " => equiv
 
+/--
+Lifting by two edges that are inverses of each other does not change the word if the initial word is reduced. This is the part of homotopy lifting we need.
+-/
 theorem homotopy_lifting_endpoints(start: List (α × Bool))(a : α)(b: Bool):
     Reduced start → start :+ (a, b) :+ (a, !b) = start := by
   intro reduced_start
@@ -201,6 +254,9 @@ theorem homotopy_lifting_endpoints(start: List (α × Bool))(a : α)(b: Bool):
       rw [cnc, eqn]
       simp
 
+/--
+The next vertex of the lift starting at a reduced word is equivalent to the word with the next vertex added.
+-/
 theorem nextVertex_equiv_concat
     (start: List (α × Bool))(letter: (α × Bool)):
     start.concat letter ∼  start :+ letter  := by
@@ -222,10 +278,16 @@ theorem nextVertex_equiv_concat
       exact step'
     else
       simp [cancP, nonnil_nextVertex_extend, nilP]
-        
+
+/--
+The final vertex on lifting a word starting at an initial (reduced) word.
+-/        
 def finalVertex (start word : List (α × Bool)) : List (α × Bool) :=
   List.foldl (nextVertex) start word
 
+/--
+The final vertex of a lift starting at a reduced word is reduced.
+-/
 theorem finalVertex_reduced (start word : List (α × Bool)) :
     Reduced start → Reduced (finalVertex start word) := 
   fun reduced_start ↦
@@ -235,6 +297,9 @@ theorem finalVertex_reduced (start word : List (α × Bool)) :
     finalVertex_reduced (start :+ head) tail 
       (nextVertex_reduced start head reduced_start)
 
+/--
+If words are equivalent then the results of appending a word to them are equivalent.
+-/
 theorem append_equiv {L₁ L₂ : List (α × Bool)}(L₃ : List (α × Bool)) :
     L₁ ∼ L₂ → L₁ ++ L₃ ∼ L₂ ++ L₃ := by
   intro eqn
@@ -242,6 +307,9 @@ theorem append_equiv {L₁ L₂ : List (α × Bool)}(L₃ : List (α × Bool)) :
   rw [← mul_mk, ← mul_mk]
   rw [eqn]
      
+/--
+The final vertex of a lift starting at a reduced word is equivalent to the result of appending the word to the initial word.
+-/
 theorem finalVertex_equiv_append (start word : List (α × Bool)) :
     start ++ word ∼ finalVertex start word  := by
   match word with
@@ -256,7 +324,9 @@ theorem finalVertex_equiv_append (start word : List (α × Bool)) :
     let eqn' := append_equiv tail eqn
     simp [← eqn']
       
-
+/--
+Induced map on the free group corresponding to lifting. The existence and naturality show that equivalent words lift to paths with the same endpoint.
+-/
 def finalVertexOnFreeGroup (start : List (α × Bool)) (red : Reduced start) 
     (g: FreeGroup α) : List (α × Bool) := by
   apply Quot.liftOn g (finalVertex start)
@@ -272,12 +342,22 @@ def finalVertexOnFreeGroup (start : List (α × Bool)) (red : Reduced start)
   apply finalVertex_reduced
   assumption
 
+/--
+Naturality of the induced map on the free group corresponding to lifting.
+-/
 theorem finalVertexOnFreeGroup_natural (start word : List (α × Bool)) 
     (red : Reduced start) : 
   finalVertex start word = finalVertexOnFreeGroup start red (mk word) := by rfl
 
-abbrev reducedWord (g : FreeGroup α) := finalVertexOnFreeGroup [] reduced_nil g
+/--
+We map an element of the free group to a reduced word.
+-/
+abbrev reducedWord (g : FreeGroup α) := 
+  finalVertexOnFreeGroup [] reduced_nil g
 
+/--
+The reduced word to which an element in the free group maps represents the element.
+-/
 theorem reducedWord_inverse 
     (g : FreeGroup α) : mk (reducedWord g) = g := by
   apply Quot.ind (β := fun g ↦ mk (reducedWord g) = g )
