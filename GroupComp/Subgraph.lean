ae20ed86962f5da69d8b9c73d3248eb465392166
ART0 <18333981+0Art0@users.noreply.github.com>
2023-07-11 10:35:52 +0530

    Define spanning sub-trees

--- GroupComp/Subgraph.lean
+++ GroupComp/Subgraph.lean
@@ -13,16 +13,21 @@ namespace Subgraph
 
 variable {V E : Type _} (G : Graph V E) (H : Subgraph G)
 
-attribute [aesop safe apply] edges_bar edges_init
+-- attribute [aesop safe apply] edges_bar edges_init
 
-@[simp] theorem edges_term : ∀ e ∈ H.edges, G.τ e ∈ H.verts := by
+theorem edges_term : ∀ e ∈ H.edges, G.τ e ∈ H.verts := by
   intro e he 
   rw [← G.ι_bar e]
   apply H.edges_init
   apply H.edges_bar
   exact he
 
-def coe : Graph H.verts H.edges where
+theorem bar_edges (e : E) (ebH : G.bar e ∈ H.edges) : e ∈ H.edges := by
+  rw [← G.bar_involution e]
+  apply edges_bar
+  assumption
+
+def coe {G : Graph V E} (H : Subgraph G) : Graph H.verts H.edges where
   ι := fun ⟨e, eH⟩ ↦ ⟨G.ι e, H.edges_init _ eH⟩
   bar := fun ⟨e, eH⟩ ↦ ⟨G.bar e, H.edges_bar _ eH⟩
   bar_involution := by intro; simp only [Graph.bar_involution]
@@ -30,6 +35,19 @@ def coe : Graph H.verts H.edges where
 
 end Subgraph
 
+structure PreconnectedSubgraph (G : Graph V E) extends Subgraph G where
+  pathBetween : (u v : verts) → (Subgraph.coe _).EdgePath u v
+
+structure Subtree (G : Graph V E) extends PreconnectedSubgraph G where
+  path_unique : ∀ u v : verts, ∀ p : (Subgraph.coe _).EdgePath u v, 
+    [[p]] = [[pathBetween u v]]
+
+theorem Subtree.pathBetween_inv (Γ : Subtree G) (u v : Γ.verts) : 
+    Γ.coe.FundamentalGroupoid.comp [[Γ.pathBetween u v]] [[Γ.pathBetween v u]] = Γ.coe.FundamentalGroupoid.id u := by
+  show [[_ ++ _]] = [[_]]
+  trans
+  · apply Γ.path_unique
+  · symm; apply Γ.path_unique
 
 structure Graph.hom {V E V' E' : Type _} (G : Graph V E) (G' : Graph V' E') where
   vertexMap : V → V'
@@ -61,7 +79,8 @@ def pathMap {u v : V} : G.EdgePath u v → G'.EdgePath (φ.vertexMap u) (φ.vert
   | .nil _ => .nil _
   | .cons e p => .cons (φ.edgeBetweenMap e) (pathMap p)
 
-def pathMap_append {u v w : V} (p : G.EdgePath u v) (p' : G.EdgePath v w) : φ.pathMap (p ++ p') = φ.pathMap p ++ φ.pathMap p' := by
+def pathMap_append {u v w : V} (p : G.EdgePath u v) (p' : G.EdgePath v w) : 
+    φ.pathMap (p ++ p') = φ.pathMap p ++ φ.pathMap p' := by
   induction p with
     | nil _ => rfl
     | cons e p ih => 
@@ -69,6 +88,8 @@ def pathMap_append {u v w : V} (p : G.EdgePath u v) (p' : G.EdgePath v w) : φ.p
       congr
       apply ih
 
+instance : @CategoryTheory.Functor V G.FundamentalGroupoid.toCategory V' G'.FundamentalGroupoid.toCategory := sorry
+  
 end Graph.hom
 
 structure SpanningSubgraph {V E : Type _} (G : Graph V E) extends Subgraph G where
@@ -77,3 +98,15 @@ structure SpanningSubgraph {V E : Type _} (G : Graph V E) extends Subgraph G whe
 def SpanningSubgraph.coe {V E : Type _} {G : Graph V E} (H : SpanningSubgraph G) : Graph V H.edges :=
   let H' := H.spanning ▸ H.toSubgraph.coe
   { H' with ι := Subtype.val ∘ H'.ι }
+
+structure SpanningSubtree (G : Graph V E) extends SpanningSubgraph G, Subtree G
+
+namespace SpanningSubtree
+
+variable {V E : Type _} (G : Graph V E) (Γ : SpanningSubtree G)
+
+def coe := Γ.toSpanningSubgraph.coe
+
+end SpanningSubtree
+
+def Graph.spanningTree (G : Graph V E) : SpanningSubtree G := sorry
