fbf73681e03ca61be9ad4bf00d7b4208751b5b92
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-07-19 12:34:20 +0530

    steps to quotienting

--- GroupComp/Graph/SubgroupCover.lean
+++ GroupComp/Graph/SubgroupCover.lean
@@ -55,7 +55,7 @@ theorem relH_trans {τ : V} {v₁ v₂ v₃ : EdgePath G x₀ τ} :
     rw [← append_commutes, ← reverse_commutes] 
     exact h₃ 
 
-scoped instance vertSetoid : Setoid (Vert G x₀) where
+scoped instance vertSetoid  : Setoid (Vert G x₀) where
   r := fun ⟨τ₁, v₁, _⟩ ⟨τ₂, v₂, _⟩ => by
     if c:τ₁=τ₂ then
       cases c
@@ -85,3 +85,91 @@ scoped instance vertSetoid : Setoid (Vert G x₀) where
           simp [c₂]
       else
         simp [c₁]
+
+theorem terminal_eq_of_r {v₁ v₂ : Vert G x₀ } :
+  vertSetoid H |>.r v₁ v₂ → v₁.τ = v₂.τ := 
+  by
+    intro h
+    simp [Setoid.r] at h
+    by_cases c:v₁.τ=v₂.τ <;> simp [c] at h 
+    simp [c]
+
+theorem path_eq_iff_r {τ : V}(p₁ p₂ : EdgePath G x₀ τ)
+  {red₁ : reduced p₁} {red₂ : reduced p₂} :
+  (vertSetoid H |>.r ⟨τ, p₁, red₁⟩ ⟨τ, p₂, red₂⟩) ↔ 
+      [[ p₁ ++ p₂.reverse ]] ∈ H := by 
+    simp [Setoid.r, relH]
+
+scoped instance edgeSetoid : Setoid (Edge G x₀) where
+  r := fun ⟨τ₀, τ₁, e, p, _⟩ ⟨τ₀', τ₁', e', p', _⟩ => by
+    if c:τ₀=τ₀' ∧ τ₁ = τ₁' then
+      cases c.left
+      cases c.right
+      exact (relH H p p') ∧ (relH H (p :+ e) (p' :+ e')) 
+    else
+      exact False
+  iseqv := by
+    apply Equivalence.mk
+    · intro ⟨τ₀, τ₁, e, p, _⟩ 
+      simp only [relH_refl, dite_eq_ite]
+    · intro ⟨τ₀, τ₁, e, p, _⟩ ⟨τ₀', τ₁', e', p', _⟩
+      if c:τ₀=τ₀' ∧ τ₁ = τ₁' then
+      cases c.left
+      cases c.right
+      simp only [dite_eq_ite, ite_true]
+      intro hyp
+      apply And.intro
+      · apply relH_symm H hyp.1
+      · apply relH_symm H hyp.2
+    else
+      simp only [c, dite_false, IsEmpty.forall_iff]
+    · intro ⟨τ₀, τ₁, e, p, _⟩ ⟨τ₀', τ₁', e', p', _⟩ ⟨τ₀'', τ₁'', e'', p'', _⟩ 
+      if c₁:τ₀=τ₀' ∧ τ₁ = τ₁' then
+        cases c₁.left
+        cases c₁.right
+        simp
+        if c₂:τ₀=τ₀'' ∧ τ₁ = τ₁'' then
+          cases c₂.left
+          cases c₂.right
+          simp 
+          intro hyp₁ hyp₂ hyp₃ hyp₄
+          apply And.intro
+          · apply relH_trans H hyp₁ hyp₃
+          · apply relH_trans H hyp₂ hyp₄
+        else
+          simp [c₂]
+      else
+        simp [c₁]
+
+theorem terminal₁_eq_of_r {e₁ e₂ : Edge G x₀ } :
+  edgeSetoid H |>.r e₁ e₂ → e₁.τ₁ = e₂.τ₁ := 
+  by
+    intro h
+    simp [Setoid.r] at h
+    by_cases c:(e₁.τ₁=e₂.τ₁) <;> simp [c] at h 
+    simp [c]
+
+theorem terminal₀_eq_of_r {e₁ e₂ : Edge G x₀ } :
+  edgeSetoid H |>.r e₁ e₂ → e₁.τ₀ = e₂.τ₀ := 
+  by
+    intro h
+    simp [Setoid.r] at h
+    by_cases c:(e₁.τ₀=e₂.τ₀) <;> simp [c] at h 
+    simp [c]
+
+def QuotVert  := Quotient (vertSetoid H)
+def QuotEdge  := Quotient (edgeSetoid H)
+
+def QuotVert.ι : Quotient (edgeSetoid H) → Quotient (vertSetoid H) := by
+  apply Quotient.lift (⟦ (G.univ x₀).ι ·⟧)
+  intro ⟨τ₀, τ₁, e, p, pf⟩ ⟨τ₀', τ₁', e', p', pf'⟩ h
+  let lem₀ := terminal₁_eq_of_r H h
+  let lem₁ := terminal₀_eq_of_r H h
+  simp at lem₁
+  simp at lem₀
+  cases lem₀
+  cases lem₁
+  sorry
+  
+
+#check Quotient
