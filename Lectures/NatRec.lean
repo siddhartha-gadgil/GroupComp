c263d6d72711cfcaf0b1acfa4ec0f1f9eec077a6
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-03-01 16:55:42 +0530

    termination_by updated

--- Lectures/NatRec.lean
+++ Lectures/NatRec.lean
@@ -30,19 +30,19 @@ These are given by the recursive definition
 
 * `fib 0 = 1`
 * `fib 1 = 1`
-* `fib (n + 2) = fib n + (fib (n + 1))` 
+* `fib (n + 2) = fib n + (fib (n + 1))`
 -/
-def fib : ℕ → ℕ 
+def fib : ℕ → ℕ
 | 0 => 1
 | 1 => 1
 | n + 2 => fib n + (fib (n + 1))
- 
+
 /-!
 The above definition is not efficient as a single computation is called many times.
 
 We can instead define pairs, so
 if  `(a, b) = (fib n, fib (n + 1))`
-then `(fib (n + 1), fib (n + 2)) = (b, a+ b)` 
+then `(fib (n + 1), fib (n + 2)) = (b, a+ b)`
 
 To define Fibonacci pairs as above, we have two choices. We can view the pair at `n` as obtained by `n` iterations of the function `g: (a, b) ↦ (b, a + b)` starting with the pair `(1,1)`. Note that we can recursively use either `g^(n + 1) = g^n ∘ g` or `g^(n + 1) = g ∘ g^n`. The former is more efficient as it means the recursive function is called at the end to give a result, with modified arguments. This allows so called _tail call optimization_, where new copies of the function are not created.
 -/
@@ -59,7 +59,7 @@ def fib'(n) := (fibAux 1 1 n).1
 The following definition is clearly wrong and will loop infinitely. Indeed if we attempt to define
 
 ```lean
-def silly_fib : ℕ → ℕ 
+def silly_fib : ℕ → ℕ
 | 0 => 1
 | 1 => 1
 | n + 2 => silly_fib n + (silly_fib (n + 3))
@@ -80,12 +80,12 @@ structural recursion cannot be used
 failed to prove termination, use `termination_by` to specify a well-founded relation
 ```
 -/
-partial def silly_fib : ℕ → ℕ 
-| 0 => 1 
+partial def silly_fib : ℕ → ℕ
+| 0 => 1
 | 1 => 1
 | n + 2 => silly_fib n + (silly_fib (n + 3))
 
-#check silly_fib -- ℕ → ℕ   
+#check silly_fib -- ℕ → ℕ
 
 /-!
 However even if functions terminate we can still get such an error. This is because Lean does not know that the function is terminating. We can fix this by adding the `partial` keyword but better still we can prove termination.
@@ -106,7 +106,7 @@ def hcf (a b : ℕ) : ℕ :=
 
 def hcf (a b : ℕ) : ℕ :=
   if b < a then hcf b a
-  else 
+  else
     if c':a = 0 then b
   else
     have : b % a < a := by
@@ -114,7 +114,7 @@ def hcf (a b : ℕ) : ℕ :=
       apply Nat.pos_of_ne_zero
       assumption
     hcf (b % a) a
-termination_by _ a b => a
+termination_by a b => a
 
 #eval hcf 8 12 -- 4
 
@@ -129,10 +129,10 @@ def sumUpto : ℕ → ℕ
 | 0 => 0
 | n + 1 => n + 1 + sumUpto n
 
-theorem sum_formula (n: ℕ) :  sumUpto n = (n * (n + 1) : ℚ) / 2  := by 
+theorem sum_formula (n: ℕ) :  sumUpto n = (n * (n + 1) : ℚ) / 2  := by
   induction n with
   | zero => rfl
-  | succ n ih => 
+  | succ n ih =>
     simp [sumUpto, ih]
     linarith
 
@@ -146,11 +146,11 @@ def sumAux (n: ℕ)(accum : ℕ) : ℕ :=
   -- partial def wrong(n: ℕ): Empty :=
 --   wrong (n + 1)
 
-axiom wrong : ℕ → Empty  
+axiom wrong : ℕ → Empty
 
-theorem vac_contra : ∀ e : Empty, ∀ n: ℕ,  
+theorem vac_contra : ∀ e : Empty, ∀ n: ℕ,
   wrong n = e →  n = 2 := by
-  intro e 
+  intro e
   induction e
 
 theorem one_eq_two : 1 = 2 := by
